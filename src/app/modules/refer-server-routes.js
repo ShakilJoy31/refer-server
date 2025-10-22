11cc74bf0f93355591def89b00a91484d15810c6
ShakilJoy31 <joy31a@gmail.com>
2025-10-22 10:14:31 +0600

    Token is done for user validation

--- src/app/modules/refer-server-routes.js
+++ src/app/modules/refer-server-routes.js
@@ -6,19 +6,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // app/modules/refer-server-routes.ts
 const express_1 = __importDefault(require("express"));
 const authentication_controller_1 = require("./authentication/authentication.controller");
-const protected_routes_1 = __importDefault(require("./protected-routes"));
 const refer_controller_1 = require("./refer/refer.controller");
 const dashboard_controller_1 = require("./dashboard/dashboard.controller");
+const auth_1 = require("../middleware/auth");
 const router = express_1.default.Router();
 // Public routes
 router.post("/signup", authentication_controller_1.signup);
 router.post("/login", authentication_controller_1.login);
-router.get("/get-user-by-id", authentication_controller_1.getUserByIdController);
+router.get("/get-user-by-id", auth_1.authenticate, authentication_controller_1.getUserByIdController);
 router.put("/update-user-by-id/:id", authentication_controller_1.updateUserController);
 // Refer
 router.put("/purchase-book", refer_controller_1.purchase);
 // Fetching dashboard data
 router.get("/get-user-refer", dashboard_controller_1.getUserDashboardById);
-// Protected routes (require JWT token)
-router.use(protected_routes_1.default);
 exports.default = router;
--- src/app/modules/refer-server-routes.ts
+++ src/app/modules/refer-server-routes.ts
@@ -1,16 +1,16 @@
 // app/modules/refer-server-routes.ts
 import express from 'express';
 import { getUserByIdController, login, signup, updateUserController } from './authentication/authentication.controller';
-import protectedRoutes from './protected-routes';
 import { purchase } from './refer/refer.controller';
 import { getUserDashboardById } from './dashboard/dashboard.controller';
+import { authenticate } from '../middleware/auth';
 
 const router = express.Router();
 
 // Public routes
 router.post("/signup", signup);
 router.post("/login", login);
-router.get("/get-user-by-id", getUserByIdController);
+router.get("/get-user-by-id", authenticate, getUserByIdController);
 router.put("/update-user-by-id/:id", updateUserController); 
 
 
@@ -21,8 +21,5 @@ router.put("/purchase-book", purchase);
 
 // Fetching dashboard data
 router.get("/get-user-refer", getUserDashboardById);
-
-// Protected routes (require JWT token)
-router.use(protectedRoutes);
 
 export default router;
